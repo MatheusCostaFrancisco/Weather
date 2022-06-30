641bc95e5cbc4dd77c85bfdc115d014a33d229b1
gianseneda <gian_seneda@hotmail.com>
2022-06-30 15:34:25 -0300

    adicionando o backgorund

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./App.css";
 import { WeatherController } from "./infra/Controller/weather.controller";
 
 function App() {
-  //const [data, setData] = useState({});
+  const [data, setData] = useState({});
 
   const loadData = async () => {
     const getData = await WeatherController.getCurrentByCity("Araras");
