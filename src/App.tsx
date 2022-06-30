5d895153e4792e4870bbd41709dc140c273dc900
gianseneda <gian_seneda@hotmail.com>
2022-06-30 15:35:10 -0300

    adicionando o backgorund

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import "./App.css";
 import { WeatherController } from "./infra/Controller/weather.controller";
