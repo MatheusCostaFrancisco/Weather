0327b8097000cdb55f305912def8a0a8dd0706c4
gianseneda <gian_seneda@hotmail.com>
2022-06-29 22:37:48 -0300

    adicionando o backgorund

--- src/App.tsx
+++ src/App.tsx
@@ -17,24 +17,7 @@ function App() {
     loadData();
   }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="background">Olá</div>;
 }
 
 export default App;
